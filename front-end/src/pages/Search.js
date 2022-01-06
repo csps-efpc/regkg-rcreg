f2b5be0ede17cfc86de74f6b62fc660e1be2f6a2
Matthew Clements <matthew.sta.clements@gmail.com>
2022-01-06 09:14:13 -0500

    Add Search Terms and Results -
    Included in this is task CU-21prutm

--- front-end/src/pages/Search.js
+++ front-end/src/pages/Search.js
@@ -9,10 +9,57 @@ import FormControl from "react-bootstrap/FormControl";
 import {FormattedMessage} from 'react-intl';
 import "../style.css";
 import logo from "../img/logo.svg";
-import Theme from "../components/Theme"
+import Theme from "../components/Theme";
+
+import {Context} from "../components/lang/LanguageWrapper";
 
 
 const Search = () => {
+
+  const langContext = useContext(Context);
+  const currentLang = langContext.locale;
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setsearchResults] = useState("");
+
+  const updateQuery = (e) => {
+    setSearchQuery(e.target.value);
+  }
+
+  const submitQuery = async() => {
+    /* Single search term: https://dev.handshape.com/search?df=text_en_txt&q=fish */
+    const protocol = "https://";
+    //const currentHost = window.location.hostname;
+    const currentHost = "dev.handshape.com";
+    const solrPath = "/search?"
+    const langTerms = `text_${currentLang}_txt`;
+    const searchTerms = `q=${searchQuery}`;
+    const requestURL = protocol + currentHost + solrPath;
+
+    fetch(requestURL + new URLSearchParams({
+        q:searchQuery,
+        df:`text_${currentLang}_txt`
+    }), {
+      method: "GET",
+      dataType: "JSON",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      }
+    })
+    .then((resp) => {
+      return resp.json()
+    }) 
+    .then((data) => {
+      setsearchResults(data.response);
+    })
+    .then(() => {
+      console.log(searchResults);
+    })
+    .catch((error) => {
+      console.log(error, "catch the blip")
+    })
+
+  }
 
   const contentTranslations = {
     title : <FormattedMessage id = "app.search.title" />,
@@ -21,6 +68,46 @@ const Search = () => {
     advancedQueries : <FormattedMessage id = "app.search.advancedQueries" />,
     referenceGuide : <FormattedMessage id = "app.search.referenceGuide" />,
     searchLabel : <FormattedMessage id = "app.search.searchLabel" />,
+  }
+
+  let searchResultJSX = "";
+  let searchResultItems = []
+
+  if (searchResults.docs){
+    for (let doc of Object.entries(searchResults.docs)) {
+      if(doc[1][`text_${currentLang}_txt`]){
+        console.log(doc[1][`text_${currentLang}_txt`][0]);
+        searchResultItems.push(
+          <Container className="slight-border px-5 py-3 mb-2 rounded-3">
+            <Row><h2>{doc[1][`title_${currentLang}_txt`]}</h2></Row>
+            <Row>
+              <Button variant="light" className="left-button" size="lg" data-link="{doc[1].id}">
+                <span class="material-icons inline-icon-large">chevron_right</span>
+                Related Regulations
+              </Button>
+            </Row>
+            {/*<Row><p>{(doc[1][`text_${currentLang}_txt`]).toString().slice(0, 150) + "..."}</p></Row>*/}
+            <Row>
+              <p>{(doc[1][`text_${currentLang}_txt`])}</p>
+            </Row>
+            <Row>
+              <Button variant="light" className="left-button" size="lg" data-link="{doc[1].id}">
+                <span class="material-icons inline-icon-large">open_in_new</span> Open Regulation In New Tab
+              </Button>
+            </Row>
+          </Container>
+        );
+      }
+    };
+    searchResultJSX = <>
+      <hr/>
+      <Container className="p-5 mb-4 bg-light rounded-3">
+        <Row>
+          {searchResultItems}
+        </Row>
+      </Container>
+
+    </>;
   }
 
   return(
@@ -41,20 +128,18 @@ const Search = () => {
 
           {/*Search Box*/}
           <InputGroup size="lg">
-            <InputGroup.Text id="inputGroup-sizing-lg">{contentTranslations.searchLabel}</InputGroup.Text>
-            <FormControl aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
+            <FormControl aria-label="Large" aria-describedby="inputGroup-sizing-lg" onChange={updateQuery}/>
+            <Button variant="primary" id="search-button" onClick={submitQuery}>
+              {contentTranslations.mainButton}
+            </Button>
           </InputGroup>
 
         {/*Reference Guide*/}
           <p></p> 
           <p>{contentTranslations.advancedQueries} <a href="http://www.solrtutorial.com/solr-query-syntax.html"> {contentTranslations.referenceGuide} </a></p>
         </Row>
-
-        {/*Sub Header*/}
-        <Row className="justify-content-md-center">
-          <Button md="auto"><h2 className="display-6 bg-primary rounded-3 p-2">{contentTranslations.mainButton}</h2></Button>
-        </Row>
       </Container>
+      {searchResultJSX}
     </Theme>
   );
 }
