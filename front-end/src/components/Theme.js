05441d6da9303600c59481d9254672dac0124c35
Matthew Clements <matthew.sta.clements@gmail.com>
2022-01-11 11:18:53 -0500

    add better documentation

--- front-end/src/components/Theme.js
+++ front-end/src/components/Theme.js
@@ -2,6 +2,11 @@ import React, { useState, useContext } from "react";
 import Container from "react-bootstrap/Container";
 import Navigation from "../components/Navigation"
 
+/*
+  Pages of Use: All
+  Description: Provides an easy way for all of the pages to have a similar look and feel.
+  Implements the navigation bar so that each page does not need to.
+*/
 
 const Theme = (props) => {
   return(
