cd6596bfc72eb0bb09205ef2e9cb19eb6d3a6266
Joshua Turner <jturner@handshape.com>
2022-01-20 11:35:08 -0500

    CU-21prute - Make API endpoint configurable via Docker

--- front-end/src/components/search/result/Information.js
+++ front-end/src/components/search/result/Information.js
@@ -1,123 +1,137 @@
-import React, { useEffect, useState } from "react";
-import {FormattedMessage} from 'react-intl';
+import React, { useEffect, useState }
+from "react";
+import {FormattedMessage}
+from 'react-intl';
 import Button from "react-bootstrap/Button";
 /*
-  Pages of Use: Search
-  Description: View of the reg info and related regs.
-  By default it is a button on its own
-  When clicked it sends a query to the search API
-  This updates the RegInfo state
-  New UI elements to show this info pop up
+ Pages of Use: Search
+ Description: View of the reg info and related regs.
+ By default it is a button on its own
+ When clicked it sends a query to the search API
+ This updates the RegInfo state
+ New UI elements to show this info pop up
+ 
+ props:
+ id: a string identifying the exact regulatory instrument
+ example: https://www.canada.ca/en/privy-council/ext/statutory-instrument/P-15.6
+ example 2: https://www.canada.ca/en/privy-council/ext/statutory-instrument/P-15.6#1423 where the #1423 is the id for the block within the regulation
+ 
+ if there is a # symbol, be aware to split the string at it and take the text before the hash
+ if there is not, do not split
+ 
+ */
 
-  props:
-    id: a string identifying the exact regulatory instrument
-      example: https://www.canada.ca/en/privy-council/ext/statutory-instrument/P-15.6
-      example 2: https://www.canada.ca/en/privy-council/ext/statutory-instrument/P-15.6#1423 where the #1423 is the id for the block within the regulation
+const Information = (props) => {
 
-      if there is a # symbol, be aware to split the string at it and take the text before the hash
-      if there is not, do not split
+    const predicates = [// Declare the set of predicates that we'll be generating programmatically. The justice ones are all made-up.
+        //Regex for find: final PropertyImpl .* = new PropertyImpl\(
+        "https://laws-lois.justice.gc.ca/ext/instrument-id",
+        "https://www.gazette.gc.ca/ext/sponsor",
+        "https://www.gazette.gc.ca/ext/consultation-word-count",
+        "https://schema.org/wordCount",
+        "https://laws-lois.justice.gc.ca/ext/section-count",
+        "https://www.gazette.gc.ca/ext/cba-word-count",
+        "https://www.gazette.gc.ca/ext/rias-word-count",
+        "https://laws-lois.justice.gc.ca/ext/enabling-act",
+        "https://laws-lois.justice.gc.ca/ext/amends-instrument",
+        "https://laws-lois.justice.gc.ca/ext/consolidates",
+        "https://laws-lois.justice.gc.ca/ext/enables-regulation",
+        "https://schema.org/name",
+        "https://schema.org/url",
+        "https://www.tpsgc-pwgsc.gc.ca/recgen/ext/org-name",
+        "https://www.tpsgc-pwgsc.gc.ca/recgen/ext/department-head",
+        "https://www.csps-efpc.gc.ca/ext/instrument-references"
+    ]
 
-*/
+    const [moreInfo, setMoreInfo] = useState();
 
-const Information = (props) =>{
+    const queryStringGenerator = (id) => {
+        const querySign = "query=";
+        const selectTerms = encodeURIComponent("SELECT * {<") + id.split("#")[0] + encodeURIComponent("> ?p ?o} LIMIT 8")
+        return querySign + selectTerms;
+    }
 
-  const predicates = [ // Declare the set of predicates that we'll be generating programmatically. The justice ones are all made-up.
-    //Regex for find: final PropertyImpl .* = new PropertyImpl\(
-    "https://laws-lois.justice.gc.ca/ext/instrument-id",
-    "https://www.gazette.gc.ca/ext/sponsor",
-    "https://www.gazette.gc.ca/ext/consultation-word-count",
-    "https://schema.org/wordCount",
-    "https://laws-lois.justice.gc.ca/ext/section-count",
-    "https://www.gazette.gc.ca/ext/cba-word-count",
-    "https://www.gazette.gc.ca/ext/rias-word-count",
-    "https://laws-lois.justice.gc.ca/ext/enabling-act",
-    "https://laws-lois.justice.gc.ca/ext/amends-instrument",
-    "https://laws-lois.justice.gc.ca/ext/consolidates",
-    "https://laws-lois.justice.gc.ca/ext/enables-regulation",
-    "https://schema.org/name",
-    "https://schema.org/url",
-    "https://www.tpsgc-pwgsc.gc.ca/recgen/ext/org-name",
-    "https://www.tpsgc-pwgsc.gc.ca/recgen/ext/department-head",
-    "https://www.csps-efpc.gc.ca/ext/instrument-references"
-  ]
+    const submitSparqlRelated = async(id) => {
+        /* Single search term: https://example.com/sparql?query=SELECT%20*%20{%3Chttps://www.canada.ca/en/privy-council/ext/statutory-instrument/P-15.6%3E%20%3Chttps://schema.org/url%3E%20?o}%20LIMIT%208&Accept=application/sparql-results%2Bjson */
+        const API_PREFIX = (process.env.REACT_APP_API_PREFIX ? process.env.REACT_APP_API_PREFIX : "");
+        const spaqrlPath = "/sparql?"
+        const queryTerms = queryStringGenerator(id);
+        const acceptTerms = "&Accept=application/sparql-results%2Bjson";
+        const requestURL = API_PREFIX + spaqrlPath + queryTerms + "&Accept=application/sparql-results%2Bjson";
 
-  const [moreInfo, setMoreInfo] = useState();
+        fetch(requestURL)
+                .then((resp) => {
+                    return resp.json();
+                })
+                .then((data) => {
+                    return data.results.bindings;
 
-  const queryStringGenerator = (id) => {
-    const querySign = "query=";
-    const selectTerms =  encodeURIComponent("SELECT * {<") + id.split("#")[0] + encodeURIComponent("> ?p ?o} LIMIT 8")
-    return querySign + selectTerms;
-  }
+                })
+                .then((data) => {
+                    const dataInCurrentLang = [];
+                    for (const object of data) {
+                        if (object.o["xml:lang"] == props.language) {
+                            dataInCurrentLang.push(object);
+                        }
+                    }
+                    return dataInCurrentLang;
+                })
+                .then((data) => {
+                    let convertedObject = {}
+                    for (const object of data) {
+                        for (const p of predicates) {
+                            if (object.p.value == p)
+                                convertedObject[p] = object.o.value;
+                        }
+                    }
+                    return convertedObject;
+                })
+                .then((data) => {
+                    props.setSparqlData((prevState) => ({...prevState, [id]: data}));
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
 
-  const submitSparqlRelated = async(id) => {
-    /* Single search term: https://example.com/sparql?query=SELECT%20*%20{%3Chttps://www.canada.ca/en/privy-council/ext/statutory-instrument/P-15.6%3E%20%3Chttps://schema.org/url%3E%20?o}%20LIMIT%208&Accept=application/sparql-results%2Bjson */
-    const API_PREFIX = (process.env.NODE_ENV == "development" ? process.env.REACT_APP_API_PREFIX : ""); // If prefix is set in environment variables, append to the request, otherwise use relative path
-    const spaqrlPath = "/sparql?"
-    const queryTerms = queryStringGenerator(id);
-    const acceptTerms = "&Accept=application/sparql-results%2Bjson";
-    const requestURL = API_PREFIX + spaqrlPath +  queryTerms + "&Accept=application/sparql-results%2Bjson";
+    }
 
-    fetch(requestURL)
-    .then((resp) => {
-      return resp.json();
-    }) 
-    .then((data) => {
-      return data.results.bindings;
+    let moreInformationPanel = "";
 
-    })
-    .then((data) => {
-      const dataInCurrentLang = [];
-      for(const object of data){
-        if(object.o["xml:lang"] == props.language){
-          dataInCurrentLang.push(object);
-        }
-      }
-      return dataInCurrentLang;
-    })
-    .then((data) => {
-      let convertedObject = {}
-      for(const object of data){
-        for(const p of predicates){
-          if(object.p.value == p)
-            convertedObject[p] = object.o.value;
-        }
-      }
-      return convertedObject;
-    })
-    .then((data) => {
-      props.setSparqlData((prevState) => ({...prevState, [id]: data}));
-    })
-    .catch((error) => {
-      console.log(error)
-    })
+    if (props.sparqlData[props.id]){
+    moreInformationPanel =
+            <span className="slight-border px-5 py-1 pt-4 m-2 rounded-3">
+    {/*
+     Get all the keys for the sparqlData[props.id] object (which are predicates)
+     For each predicate return a new <p> tag
+     Example <p>Word Count: 32642</p>
+     */}
+    
 
-  }
 
-  let moreInformationPanel = "";
+    {Object.keys(props.sparqlData[props.id]).map((o, i) => {
+            return <p key={o}><FormattedMessage id={o}/>: {props.sparqlData[props.id][o]}</p>
+            })}
+</span>
+            }
 
-  if(props.sparqlData[props.id]){
-    moreInformationPanel = 
-    <span className="slight-border px-5 py-1 pt-4 m-2 rounded-3">
-      {/*
-          Get all the keys for the sparqlData[props.id] object (which are predicates)
-          For each predicate return a new <p> tag
-          Example <p>Word Count: 32642</p>
-      */}
+            return(
+            <>
+<Button variant="light" className="left-button" size="lg" onClick=
+        
+            {() => submitSparqlRelated(props.id)
+}
+            
+        >
+    <span className="material-icons inline-icon-large">chevron_right</span><FormattedMessage id = "app.result.information" />
+</Button>
 
-      {Object.keys(props.sparqlData[props.id]).map((o, i) => {
-        return <p key={o}><FormattedMessage id={o}/>: {props.sparqlData[props.id][o]}</p>
-      })}
-    </span>
-  }
 
-  return(
-    <>
-      <Button variant="light" className="left-button" size="lg" onClick={() => submitSparqlRelated(props.id)}>
-        <span className="material-icons inline-icon-large">chevron_right</span><FormattedMessage id = "app.result.information" />
-      </Button>
-      {moreInformationPanel}
-    </>
-  );
+            {moreInformationPanel
+}
+            
+
+</>
+);
 }
 
 export default Information;
--- front-end/src/components/search/result/View.js
+++ front-end/src/components/search/result/View.js
@@ -2,68 +2,68 @@ import React from "react";
 import {FormattedMessage} from 'react-intl';
 import Button from "react-bootstrap/Button";
 /*
-  Pages of Use: Search
-  Description: Button to open the result in a new tab.
-  Sends a query to the API to get the required URL
-  Sets the state with that URL if the button is pressed again
-
-  props:
-    id: a string identifying the exact regulatory instrument
-      example: https://www.canada.ca/en/privy-council/ext/statutory-instrument/P-15.6
-      example 2: https://www.canada.ca/en/privy-council/ext/statutory-instrument/P-15.6#1423 where the #1423 is the id for the block within the regulation
-
-      if there is a # symbol, be aware to split the string at it and take the text before the hash
-      if there is not, do not split
-*/
+ Pages of Use: Search
+ Description: Button to open the result in a new tab.
+ Sends a query to the API to get the required URL
+ Sets the state with that URL if the button is pressed again
+ 
+ props:
+ id: a string identifying the exact regulatory instrument
+ example: https://www.canada.ca/en/privy-council/ext/statutory-instrument/P-15.6
+ example 2: https://www.canada.ca/en/privy-council/ext/statutory-instrument/P-15.6#1423 where the #1423 is the id for the block within the regulation
+ 
+ if there is a # symbol, be aware to split the string at it and take the text before the hash
+ if there is not, do not split
+ */
 
 const View = (props) => {
 
-  const queryStringGenerator = (id) => {
-    const querySign = "query=";
-    const selectTerms = encodeURIComponent("SELECT * {") + "" + encodeURIComponent("<") + id.split("#")[0] + encodeURIComponent("> <") + "https://schema.org/url" + encodeURIComponent("> ") + "?o}" + encodeURIComponent(" LIMIT 8");
-    return querySign + selectTerms;
-  }
+    const queryStringGenerator = (id) => {
+        const querySign = "query=";
+        const selectTerms = encodeURIComponent("SELECT * {") + "" + encodeURIComponent("<") + id.split("#")[0] + encodeURIComponent("> <") + "https://schema.org/url" + encodeURIComponent("> ") + "?o}" + encodeURIComponent(" LIMIT 8");
+        return querySign + selectTerms;
+    }
 
-  const submitSparqlURL = async(id) => {
-    /* Single search term: https://example.com/sparql?query=SELECT%20*%20{%3Chttps://www.canada.ca/en/privy-council/ext/statutory-instrument/P-15.6%3E%20%3Chttps://schema.org/url%3E%20?o}%20LIMIT%208&Accept=application/sparql-results%2Bjson */
-    const API_PREFIX = (process.env.NODE_ENV == "development" ? process.env.REACT_APP_API_PREFIX : ""); // If prefix is set in environment variables, append to the request, otherwise use relative path
-    const spaqrlPath = "/sparql?"
-    const selectTerms = queryStringGenerator(id)
-    const acceptTerms = `Accept=application/sparql-results+json`;
-    const requestURL = API_PREFIX + spaqrlPath + selectTerms + "&Accept=application/sparql-results%2Bjson";
+    const submitSparqlURL = async(id) => {
+        /* Single search term: https://example.com/sparql?query=SELECT%20*%20{%3Chttps://www.canada.ca/en/privy-council/ext/statutory-instrument/P-15.6%3E%20%3Chttps://schema.org/url%3E%20?o}%20LIMIT%208&Accept=application/sparql-results%2Bjson */
+        const API_PREFIX = (process.env.REACT_APP_API_PREFIX ? process.env.REACT_APP_API_PREFIX : "");
+        const spaqrlPath = "/sparql?"
+        const selectTerms = queryStringGenerator(id)
+        const acceptTerms = `Accept=application/sparql-results+json`;
+        const requestURL = API_PREFIX + spaqrlPath + selectTerms + "&Accept=application/sparql-results%2Bjson";
 
-    fetch(requestURL)
-    .then((resp) => {
-      return resp.json();
-    }) 
-    .then((data) => {
-      return data.results.bindings
-    })
-    .then((data) => {
-      if(data[0])
-        if(data[0].o["xml:lang"] == props.language)
-          return data[0].o.value
-      if(data[1])
-        if(data[1].o["xml:lang"] == props.language)
-          return data[1].o.value
-    })
-    .then((data) => {
-      if(id.includes("#")){
-        window.open(data + "#" + id.split("#")[1], '_blank').focus();
-      } else {
-        window.open(data, '_blank').focus();
-      }
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  }
-  
-  return(
-    <Button variant="light" className="left-button" size="lg" onClick={() => submitSparqlURL(props.id)}>
-      <span className="material-icons inline-icon-large">open_in_new</span><FormattedMessage id = "app.result.link" />
-    </Button>
-  );
+        fetch(requestURL)
+                .then((resp) => {
+                    return resp.json();
+                })
+                .then((data) => {
+                    return data.results.bindings
+                })
+                .then((data) => {
+                    if (data[0])
+                        if (data[0].o["xml:lang"] == props.language)
+                            return data[0].o.value
+                    if (data[1])
+                        if (data[1].o["xml:lang"] == props.language)
+                            return data[1].o.value
+                })
+                .then((data) => {
+                    if (id.includes("#")) {
+                        window.open(data + "#" + id.split("#")[1], '_blank').focus();
+                    } else {
+                        window.open(data, '_blank').focus();
+                    }
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+    }
+
+    return(
+            <Button variant="light" className="left-button" size="lg" onClick={() => submitSparqlURL(props.id)}>
+                <span className="material-icons inline-icon-large">open_in_new</span><FormattedMessage id = "app.result.link" />
+            </Button>
+            );
 }
 
 export default View;
