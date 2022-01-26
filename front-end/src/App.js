46aa3cab7df259ff84e44ae0cab175f25e562640
Matthew Clements <matthew.sta.clements@gmail.com>
2022-01-25 20:34:57 -0500

    Add language routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,15 +2,35 @@ import React, { useState, useContext } from "react";
 import { Route, Routes, BrowserRouter} from 'react-router-dom';
 import Home from "./pages/Home"
 import Search from "./pages/Search"
+import {Context} from "./components/lang/LanguageWrapper";
 
-const App = () => {
+const App = () => {  
+  const context = useContext(Context);
+  
+  const EnglishRoute = () => {
+    context.selectLanguage("en");
+    return(
+      <Routes>
+        <Route path="" element={<Home />}></Route>
+        <Route path="search" element={<Search />}></Route>
+      </Routes>
+    )
+  }
+  const FrenchRoute = () => {
+    context.selectLanguage("fr");
+    return(
+      <Routes>
+        <Route path="" element={<Home />}></Route>
+        <Route path="search" element={<Search />}></Route>
+      </Routes>
+    )
+  }
 
   return(
     <BrowserRouter>
       <Routes >
-        <Route exact path="/" element={<Home />}></Route>
-        <Route exact path="/search" element={<Search />}></Route>
-        <Route path="/regkg/" element={<Home />}></Route>
+        <Route path="/en/*" element={<EnglishRoute />}></Route>
+        <Route path="/fr/*" element={<FrenchRoute />}></Route>
       </Routes >
     </BrowserRouter>
   );
