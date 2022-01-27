eb2cbc48529cb20b1174d0987c618578210f1702
Joshua Turner <jturner@handshape.com>
2022-01-27 10:20:58 -0500

    CU-21prurx - Add basic test coverage

--- src/test/java/ca/gc/csps/regkg/CsvToRdfParserTest.java
+++ src/test/java/ca/gc/csps/regkg/CsvToRdfParserTest.java
@@ -5,6 +5,10 @@ import java.net.URISyntaxException;
 import java.nio.file.Paths;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.ResourceFactory;
+import org.apache.jena.rdf.model.impl.PropertyImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -12,15 +16,15 @@ import org.junit.jupiter.api.Test;
  * @author jturner
  */
 public class CsvToRdfParserTest {
-    
+
     public CsvToRdfParserTest() {
     }
-    
+
     //Utility method to get resource files. Only works at test-time.
     private File getResourceFile(String spec) throws URISyntaxException {
-    return Paths.get(getClass().getResource("test1.csv").toURI()).toFile();
+        return Paths.get(getClass().getResource(spec).toURI()).toFile();
     }
-    
+
     /**
      * Test of parse method, of class CsvToRdfParser.
      */
@@ -32,8 +36,26 @@ public class CsvToRdfParserTest {
         CsvToRdfParser instance = new CsvToRdfParser();
         instance.parse(inputFile, model);
         model.write(System.out, "TTL");
-///assertTrue(model.listStatements(rsrc, prprt, string));
+        Resource eenie = ResourceFactory.createResource("test1:eenie");
+        Resource meenie = ResourceFactory.createResource("test1:meenie");
+        Resource mynie = ResourceFactory.createResource("test1:mynie");
+        Resource moe = ResourceFactory.createResource("test1:moe");
+        Assertions.assertTrue(model.contains(
+                eenie,
+                new PropertyImpl("https://schema.org/name"),
+                "The OG") , "Insertion of simple string properties");
+        Assertions.assertTrue(model.contains(
+                meenie,
+                new PropertyImpl("https://schema.org/url"),
+                "https://www.youtube.com/watch?v=oavMtUWDBTM"), "Insertion of schema.org URL property"); 
+        Assertions.assertTrue(model.contains(
+                mynie,
+                new PropertyImpl("custom:rank"),
+                model.createTypedLiteral(3.0D)), "Insertion of numeric properties as double literals");
+        Assertions.assertTrue(model.contains(
+                moe,
+                new PropertyImpl("test1:ext/loop"),
+                eenie), "Insertion of entity-to-entity properties");
     }
 
-    
 }
