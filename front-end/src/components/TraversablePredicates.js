5341d56cc38ef63889cff2c1e3cfac93214b36b7
Joshua Turner <jturner@handshape.com>
2022-02-24 12:05:52 -0500

    CU-21ge3tp - Make OIC to reg mappings bidirectional

--- front-end/src/components/TraversablePredicates.js
+++ front-end/src/components/TraversablePredicates.js
@@ -4,4 +4,5 @@ export const TraversablePredicates = [
       "https://schema.org/legislationConsolidates",
       "https://schema.org/legislationChanges",
       "https://laws-lois.justice.gc.ca/ext/order-implements",
+      "https://laws-lois.justice.gc.ca/ext/enabling-order",
 ] // Link Array is a list of values that can be used to 'walk the graph'
