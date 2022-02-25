82360e3ffbf59cf285ac9cb924f0feb069fb7593
Joshua Turner <jturner@handshape.com>
2022-02-25 10:37:41 -0500

    CU-21ge3tp - add normalization code for the P.C. - OIC format mismatch (#86)
    Co-authored-by: jturner <jturner@minint-luun105.localdomain>

---
+++ src/test/java/ca/gc/csps/regkg/RdfGatheringAgentTest.java
@@ -0,0 +1,36 @@
+package ca.gc.csps.regkg;
+
+import java.io.File;
+import java.util.Map;
+import java.util.Set;
+import org.apache.jena.rdf.model.Model;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for parts of the RdfGathering agent. Unit coverage supports the IntegrationTest coverage that exercises most everything.
+ * @author jturner
+ */
+public class RdfGatheringAgentTest {
+    
+    public RdfGatheringAgentTest() {
+    }
+    
+    /**
+     * Test of normalizeOICNumber method, of class RdfGatheringAgent.
+     */
+    @Test
+    public void testNormalizeOICNumber() {
+        System.out.println("normalizeOICNumber");
+        RdfGatheringAgent instance = new RdfGatheringAgent();
+        assertEquals("1234-5678", instance.normalizeOICNumber("1234-5678"));
+        assertEquals("1234-0567", instance.normalizeOICNumber("1234-567"));
+        // Em-dashes in data.
+        assertEquals("1987-0584", instance.normalizeOICNumber("1987–584"));
+    }
+
+}
